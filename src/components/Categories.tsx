1a3fd28419d94597e4bbec4acac58d83a04ccf1e
Alvin Pauly <alvinpaul1311@gmail.com>
2021-08-28 16:59:52 +0530

    Categories can be displayed

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { Category, Item } from "../models";
 
-export const Categories: React.FC = () => {
-  return (
-    <div>
-      <h2>Categories</h2>
-      <ul>
-        <li>Links 1</li>
-      </ul>
-    </div>
-  );
+interface Props {
+  lynks: Category[];
+}
+
+export const Categories: React.FC<Props> = ({ lynks }) => {
+  const displayItems = (item: Item) => {
+    return (
+      <div>
+        <a href={item.link}>{item.title}</a>
+      </div>
+    );
+  };
+
+  const displayCategories = (category: Category) => {
+    return (
+      <div>
+        <h2>{category.name}</h2>
+        <ul>{category.items.map(displayItems)}</ul>
+      </div>
+    );
+  };
+
+  return <div>{lynks.map(displayCategories)}</div>;
 };
 
 export default Categories;
--- src/components/Mains.tsx
+++ src/components/Mains.tsx
@@ -3,13 +3,48 @@ import { Category } from "../models";
 import Categories from "./Categories";
 
 interface State {
-  lynks: Array<Category>;
+  lynks: Category[];
 }
 
-export const Mains: React.FC = () => {
-  const [data, setData] = useState<State>({ lynks: [] });
+const testData: State = {
+  lynks: [
+    {
+      name: "TimePass",
+      items: [
+        {
+          title: "Youtube",
+          link: "https://www.youtube.com/",
+        },
+        {
+          title: "Netflix",
+          link: "https://www.netflix.com",
+        },
+      ],
+    },
+    {
+      name: "Study",
+      items: [
+        {
+          title: "Youtube",
+          link: "https://www.youtube.com/",
+        },
+        {
+          title: "GFG",
+          link: "https://www.geeksforgeeks.com",
+        },
+      ],
+    },
+  ],
+};
 
-  return <Categories />;
+export const Mains: React.FC = () => {
+  const [data, setData] = useState<State>({ lynks: testData.lynks });
+
+  return (
+    <div>
+      <Categories lynks={data.lynks} />
+    </div>
+  );
 };
 
 export default Mains;
