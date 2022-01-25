726a178f35fe4dff122dff1ad7d8a094b2a9f839
Alvin Pauly <alvinpaul1311@gmail.com>
2022-01-25 15:43:04 +0530

    ⬆️ Upgraded react router dependency to v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import { UserInterface } from "./utils/models";
 
 function App() {
+  const [user, setUser] = useState<UserInterface | null>();
+
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Home} />
-      <Route path='/login' exact component={Login} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
